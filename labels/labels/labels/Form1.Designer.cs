cf427ac912d657def7c7c6fade7e07ac37e4e3f8
Lauhard Andreas <34344723+01100010yteWizzard@users.noreply.github.com>
2019-04-19 21:32:15 +0200

    Add files via upload

---
+++ labels/labels/labels/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace labels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lbl_dynamic = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnOpenModalView = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.flowLayoutPanel1.Controls.Add(this.lbl_dynamic);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(501, 459);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // lbl_dynamic
+            // 
+            this.lbl_dynamic.AutoSize = true;
+            this.lbl_dynamic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lbl_dynamic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_dynamic.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dynamic.Location = new System.Drawing.Point(10, 10);
+            this.lbl_dynamic.Margin = new System.Windows.Forms.Padding(10, 10, 3, 0);
+            this.lbl_dynamic.Name = "lbl_dynamic";
+            this.lbl_dynamic.Size = new System.Drawing.Size(265, 24);
+            this.lbl_dynamic.TabIndex = 1;
+            this.lbl_dynamic.Text = "i bims a text mit variabler länge";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 44);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 10, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "i bims a fixer text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(6, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "// flowlayoutpanel";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.13966F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.86034F));
+            this.tableLayoutPanel1.Controls.Add(this.btnOpenModalView, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(759, 459);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnOpenModalView
+            // 
+            this.btnOpenModalView.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOpenModalView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOpenModalView.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenModalView.Location = new System.Drawing.Point(504, 3);
+            this.btnOpenModalView.Name = "btnOpenModalView";
+            this.btnOpenModalView.Size = new System.Drawing.Size(252, 39);
+            this.btnOpenModalView.TabIndex = 5;
+            this.btnOpenModalView.Text = "open modal window";
+            this.btnOpenModalView.UseVisualStyleBackColor = true;
+            this.btnOpenModalView.Click += new System.EventHandler(this.btnOpenModalView_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 458);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lbl_dynamic;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnOpenModalView;
+    }
+}
+
---
+++ labels/labels/labels/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labels
+{
+    public partial class Form1 : Form
+    {
+        Modal modal;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resize();
+        }
+
+        private void Modal_GetInput(string txt)
+        {
+            if(!string.IsNullOrEmpty(txt))
+                lbl_dynamic.Text = txt; 
+        }
+
+        private void btnOpenModalView_Click(object sender, EventArgs e)
+        {
+            modal = new Modal(lbl_dynamic.Text);
+            modal.Show();
+            modal.GetInput += Modal_GetInput;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resize();
+        }
+
+        private void resize()
+        {
+            tableLayoutPanel1.Height = this.Height;
+        }
+    }
+}
