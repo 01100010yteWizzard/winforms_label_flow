577c605e251c1c3e2074e784f333d3ccc4a9a35d
Lauhard Andreas <34344723+01100010yteWizzard@users.noreply.github.com>
2019-04-19 21:35:04 +0200

    Add files via upload

--- labels/labels/modal.Designer.cs
+++ labels/labels/modal.Designer.cs
@@ -40,7 +40,7 @@
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
             this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtInput.Size = new System.Drawing.Size(312, 87);
+            this.txtInput.Size = new System.Drawing.Size(507, 158);
             this.txtInput.TabIndex = 3;
             this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
             // 
@@ -50,7 +50,7 @@
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmit.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnSubmit.Location = new System.Drawing.Point(67, 109);
+            this.btnSubmit.Location = new System.Drawing.Point(162, 189);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(90, 41);
             this.btnSubmit.TabIndex = 4;
@@ -63,7 +63,7 @@
             this.bnCancel.BackColor = System.Drawing.Color.Red;
             this.bnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.bnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bnCancel.Location = new System.Drawing.Point(177, 109);
+            this.bnCancel.Location = new System.Drawing.Point(303, 189);
             this.bnCancel.Name = "bnCancel";
             this.bnCancel.Size = new System.Drawing.Size(83, 41);
             this.bnCancel.TabIndex = 5;
@@ -75,7 +75,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 162);
+            this.ClientSize = new System.Drawing.Size(531, 261);
             this.Controls.Add(this.bnCancel);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.txtInput);
